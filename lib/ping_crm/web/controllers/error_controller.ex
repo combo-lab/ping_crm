49f1b75d9ffcd2976b84ced6fcd42a7e09402410
Zeke Dou <zeke@zekedou.live>
2025-11-04 15:22:16 +0800

    fix: fix bad status code in error page

--- lib/ping_crm/web/controllers/error_controller.ex
+++ lib/ping_crm/web/controllers/error_controller.ex
@@ -2,7 +2,10 @@ defmodule PingCRM.Web.ErrorController do
   use PingCRM.Web, :controller
 
   def not_found(conn, _params) do
+    status = 404
+
     conn
-    |> inertia_render("Error", %{status: 404})
+    |> put_status(status)
+    |> inertia_render("Error", %{status: status})
   end
 end
